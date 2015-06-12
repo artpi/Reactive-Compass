b7cf2f527989486b0663c5f05ebd39c89e12a6df
unknown <a.piszek@AVDC616.DIGITAL.local>
2015-06-12 17:11:33 +0200

    Added JSDOC

--- navigationController.js
+++ navigationController.js
@@ -1,8 +1,25 @@
 var keyMap = require('./pcKeyMap.js');
 module.exports = {
+    /**
+     * Map for keboard keys. We can override to accomodate different key sets (TV, etc)
+     * @type {Object}
+     */
     keyMap: keyMap,
+    /**
+     * Array of focusable trees of ReactNodes. Ideally should have 1 element, but we can create many focusable layers
+     * @type {Array}
+     */
     navRoots: [],
+    /**
+     * Currently focused react node
+     * @type {ReactNode}
+     * @private
+     */
     currentFocus: null,
+    /**
+     * Focus on element
+     * @param  {ReactNode} focus ReactNode to focus
+     */
     focus: function (focus) {
         'use strict';
         if (this.currentFocus) {
@@ -12,6 +29,11 @@ module.exports = {
         this.currentFocus = focus;
         this.currentFocus.setState({focused: true});
     },
+    /**
+     * Move current focus
+     * @param  {number} dir  Horizontal offset from current position
+     * @param  {number} dirY Vertical offset from current position
+     */
     navMove: function (dir, dirY) {
         'use strict';
         var newEl;
@@ -22,6 +44,11 @@ module.exports = {
             }
         }
     },
+    /**
+     * Bind events and initialize
+     * @param  {rootElement} element root element for capturing events. Should be document
+     * @param  {number} rootToFocus id of root React element to find focus. should be 0.
+     */
     init: function (element, rootToFocus) {
         'use strict';
         element.addEventListener('keydown', this.handleKey.bind(this));
@@ -33,6 +60,10 @@ module.exports = {
             }
         }
     },
+    /**
+     * Dispatcher for handling keyDown event. Should be bound to document.
+     * @param  {browserEvent} event
+     */
     handleKey: function (event) {
         'use strict';
         var keyCode = keyMap[event.keyCode];
